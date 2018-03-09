81c1a46f2cb948f693f3bf26ccc32628d96e6b38
HaroldDLeon <harolddleon@users.noreply.github.com>
2018-03-09 18:35:57 -0500

    Initial commit of Animation class.

---
+++ src/Animation.java
@@ -0,0 +1,33 @@
+import java.awt.Image;
+import java.awt.Toolkit;
+
+
+public class Animation {
+	Image[] image;
+	int current = 1;
+	int delay;
+	int duration;
+	
+	
+	public Animation(String filename, int count, int duration){
+		image = new Image[count];
+		for (int i = 0; i < count; i++) {
+			image[i] = Toolkit.getDefaultToolkit().getImage(filename+i+".gif");
+		}
+		this.duration = duration;
+		delay = duration;
+	}
+	public Image getImage(){
+		if (delay == 0 ){
+			current++;
+			if (current == image.length) { current = 0;}
+			delay = duration;
+		}
+		else{
+			delay--;
+		}
+		return image[current];
+		
+	}
+	
+}
